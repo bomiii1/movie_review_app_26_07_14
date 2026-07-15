b1dc9822eea9425e16bdc6fcc2957dfb512375ad
bomiii1 <bomi9186@gmail.com>
2026-07-15 12:38:35 +0900

    data 가져오오기"

--- src/api/movieApi.js
+++ src/api/movieApi.js
@@ -3,17 +3,16 @@ const options = {
   method: "GET",
   headers: {
     accept: "application/json",
-    Authorization: `Bearer `,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMzdkOTI0OGQ4NWIzYjdkODViYzJlZDI1MzY3YTA0YyIsIm5iZiI6MTc4Mzk5OTY4OS41MjEsInN1YiI6IjZhNTVhY2M5NDAyZmNjOGE1NWVmMjRiYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.EYrz_TUS86_p9qt_LChEXJtfG75VBim8kS21fxxaDzo`,
   },
 };
 
 const fetchMovie = async (endpoint) => {
   const url = baseUrl + endpoint + "?language=en-US&page=1";
 
-  const response = await fetch(url, options)
-    .then((res) => res.json())
-    .then((json) => console.log(json))
-    .catch((err) => console.error(err));
+  const response = await fetch(url, options).then((res) => res.json());
+  // .then((json) => console.log(json))
+  // .catch((err) => console.error(err));
 
   return { response };
 };
@@ -24,3 +23,6 @@ const fetchMovie = async (endpoint) => {
 // const upcomingUrl = baseUrl + "movie/upcoming" + "language=en-US&page=1";
 
 export const getNowPlaying = () => fetchMovie("movie/now_playing");
+export const getPopular = () => fetchMovie("movie/popular");
+export const getTopRated = () => fetchMovie("movie/top_rated");
+export const getUpcoming = () => fetchMovie("movie/upcoming");
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,84 @@
-import { getNowPlaying } from "../../api/movieApi";
+import { useEffect, useState } from "react";
+import {
+  getNowPlaying,
+  getPopular,
+  getTopRated,
+  getUpcoming,
+} from "../../api/movieApi";
 
 export default function Home() {
-  const NowPlayingData = getNowPlaying();
-  console.log(NowPlayingData);
-  return <div className="w-[300px] h-[300px] bg-gray-300">home</div>;
+  // const [nowData, setNowData] = useState();
+  // const [popData, setPopData] = useState();
+  // const [topData, setTopData] = useState();
+  // const [upcomingData, setUpcomingData] = useState();
+
+  const [movieData, setMovieData] = useState({});
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // const nowPlaying = await getNowPlaying();
+        // const popular = await getPopular();
+        // const topRated = await getTopRated();
+        // const upcoming = await getUpcoming();
+
+        // setNowData(nowPlaying);
+        // setPopData(popular);
+        // setTopData(topRated);
+        // setUpcomingData(upcoming);
+
+        const [nowPlaying, popular, topRated, upcoming] = await Promise.all([
+          getNowPlaying(),
+          getPopular(),
+          getTopRated(),
+          getUpcoming(),
+        ]);
+
+        setMovieData({
+          nowPlaying,
+          popular,
+          topRated,
+          upcoming,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, []);
+
+  console.log(movieData);
+
+  // console.log(`현재 상영 영화 : ` + nowData);
+  // console.log(`인기 영화 : ` + popData);
+  // console.log(`평점 높은 영화 : ` + topData);
+  // console.log(`상영 예정 영화 : ` + upcomingData);
+
+  // *예외
+  // => try ~ catch
+  // => 조건문과 비슷하게 코드나 함수에 오류나 문제점이 발생했을때 핸들링 처리 가능함
+  // => if문과 차이점은 if문은 무조건 조건을 작성해야되지만 try는 조건없이 문제점을 잡아낼수 있음
+
+  // const [num, setNum] = useState(0);
+
+  // useEffect(() => {
+  //   console.log("랜더링될때마다 실행");
+  // });
+
+  // useEffect(() => {
+  //   console.log("num값이 변경될때마다 실행");
+  // }, [num]);
+
+  // return (
+  //   // <>
+  //   //   <p>{num}</p>
+  //   //   <button
+  //   //     onClick={() => setNum((prev) => prev + 1)}
+  //   //     className="p-4 bg-gray-300"
+  //   //   >
+  //   //     +
+  //   //   </button>
+  //   // </>
+  // );
+
+  return <div>home</div>;
 }
